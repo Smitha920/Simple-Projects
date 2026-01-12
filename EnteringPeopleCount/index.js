c22bc5f9aca80bf7f5e02282a09036efd7e508ae
Smitha M <smitha92005@gmail.com>
2026-01-12 13:48:25 +0530

    Added some features

--- EnteringPeopleCount/index.js
+++ EnteringPeopleCount/index.js
@@ -1,9 +1,16 @@
 let count = 0;
-let countEl = document.getElementById("count-el")
-
-console.log(countEl)
+let countEl = document.getElementById("count-el");
+let entryLog = document.getElementById("entries-log");
 
 function increment() {
     count += 1;
     countEl.innerText = count;
 }
+function save() {
+    let countStr = count + " - "
+    entryLog.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
+
+
