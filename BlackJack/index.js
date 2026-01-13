0aa718790701bec73bea699d53aa0fa2c5d484a6
Smitha M <smitha92005@gmail.com>
2026-01-13 22:40:35 +0530

    Added code to work button

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -3,15 +3,23 @@ let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false
 let isAlive = true;
+let message = " ";
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new game!");
-}else if ( sum === 21) {
-    console.log("Wohoo! you got a BlackJack!");
-    hasBlackJack = true;
-}else {
-    console.log("You're out of the game!");
-    isAlive = false;
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new game!"
+    }else if ( sum === 21) {
+        message = "Wohoo! you got a BlackJack!";
+        hasBlackJack = true;
+    }else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }       
 }
 
-console.log(isAlive);
+
+
+
+
+console.log(message);
