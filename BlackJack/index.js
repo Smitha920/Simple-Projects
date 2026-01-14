f84ac76a1dedc07b11a9b1215fb23806d2593f18
Smitha M <smitha92005@gmail.com>
2026-01-14 23:04:23 +0530

    Adding styling to the buttons

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,25 +1,38 @@
-let firstCard = 10;
+let firstCard = 5;
 let secondCard = 11;
+let cards = [firstCard, secondCard];
+let message = "";
 let sum = firstCard + secondCard;
 let hasBlackJack = false
 let isAlive = true;
-let message = " ";
+
+let sumEl = document.getElementById("sum-el");
+let messageEl = document.getElementById("message-el");
+let cardEl = document.getElementById("card-el");
 
 
 function startGame() {
+    renderGame();
+}
+function renderGame() {
+    //reder out firstCard and secondCard
+    cardEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    //render out ALL th cards we have
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
-        message = "Do you want to draw a new game!"
-    }else if ( sum === 21) {
-        message = "Wohoo! you got a BlackJack!";
+        message = "Do you want to draw a new game!";
+    }else if (sum === 21) {
+        message = "you got a BlackJack!";
         hasBlackJack = true;
     }else {
         message = "You're out of the game!";
         isAlive = false;
-    }       
+    }  
+    messageEl.textContent = message     ;
 }
 
-
-
-
-
-console.log(message);
+function newCard() {
+    let card = 5;
+    sum += card;
+    renderGame();
+}
